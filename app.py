52fd3d92b3c03fa558786f9b39c2da4da6251ffc
Jojo Sace <135392000+JojoSace@users.noreply.github.com>
2023-08-07 17:17:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ if (sidebar_features == 'Highest Educational Degree Completed by Household Head'
              color_discrete_sequence=px.colors.qualitative.Antique)
     fig_pie.update_traces(textposition='inside')
     fig_pie.update_layout(uniformtext_minsize=15, uniformtext_mode='hide',
-                  height=1000, title={'font': {'size': 30}}, font={'size': 20})
+                  height=1000)
     fig_pie.update_layout(barmode='group', yaxis={'categoryorder':'total descending'})
     st.plotly_chart(fig_pie, use_container_width=True)
   
